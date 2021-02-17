8e6ec28683f4df011734438c6a496009758d0ce3
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-17 16:52:40 -0500

    replaced hrefs with link and linkcontainer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
@@ -6,19 +7,25 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Pro Shop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Pro Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Item>
-                <Nav.Link href="/cart">
-                  <i className="fas fa-shopping-cart"></i>Cart
-                </Nav.Link>
+                <LinkContainer to="/cart">
+                  <Nav.Link>
+                    <i className="fas fa-shopping-cart"></i>Cart
+                  </Nav.Link>
+                </LinkContainer>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/login">
-                  <i className="fas fa-user"></i>Sign In
-                </Nav.Link>
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i>Sign In
+                  </Nav.Link>
+                </LinkContainer>
               </Nav.Item>
             </Nav>
           </Navbar.Collapse>
