238a1c0fb5a6a898927fd2a5584698103f787672
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-17 16:15:55 -0500

    created homescreen and product with basic stylings

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import products from "../products";
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            {/* Passing in the product as a prop for each product */}
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
