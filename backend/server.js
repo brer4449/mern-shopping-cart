88baef6a79f328adaae858128ab5d069d4ac5ffd
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-19 18:51:58 -0500

    added routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 const express = require("express");
 const connectDB = require("./config/db");
 const colors = require("colors");
-const products = require("./data/products");
+const productRoutes = require("./routes/productRoutes");
 dotenv.config();
 
 connectDB();
@@ -12,15 +12,7 @@ const app = express();
 
 // If there's a get request to "/", take the response and send to the client the stuff in quotes
 // app.get("/", (req, res) => res.send("API is running "));
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-// if the product id is equal to params from request object and whatever we have in the wildcard (id in our case)
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(
   PORT,
