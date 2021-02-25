3a2c572479a59f0f531e596bb5bcb7c53722cd12
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-25 14:25:55 -0500

    added cart actions and reducers, connected to store

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,27 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      // product details in this payload, including the id which is called product
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        };
+        // if it doesn't exist, just push it to array
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+    default:
+      return state;
+  }
+};
