f9a9d49af2a5bc4b047226b42947501d5283a7aa
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-25 12:59:30 -0500

    connected reducers and actions to frontend

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,19 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
+
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    // setting the products to the data (payload) || error to payload (both in payload)
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
