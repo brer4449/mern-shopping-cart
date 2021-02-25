52cb7044486ed6311d9fb8be1ff47044c9dfbf06
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-25 13:23:42 -0500

    added actions and reducer to product page

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -16,6 +19,25 @@ export const listProducts = () => async (dispatch) => {
     // checking if generic error response as well as custom error message (like the one in Postman) display them accordingly
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    // checking if generic error response as well as custom error message (like the one in Postman) display them accordingly
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
