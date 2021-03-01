47c923045d120ccce366e2de9584918e970c5d9d
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-28 23:36:21 -0500

    added GUI for saving shipping address and constants/reducer/actions

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,13 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       // product details in this payload, including the id which is called product
@@ -26,6 +33,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         ...state,
         // take out the one that we removed
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        // action.payload is going to be the data we pass in from the form
+        shippingAddress: action.payload,
       };
     default:
       return state;
